901e923224bb87a822a2eb81de2c6f6fc1679251
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-07 16:57:30 -0500

    CONDICIONAL SIMPLE 05

---
+++ Mendoza/Simple05.py
@@ -0,0 +1,16 @@
+#Para tributar un determinado impuesto se debe ser mayor a 18 años
+#tener unos ingresos superiores a 1000 soles mensuales
+#Programa que pregunte al usuario su edad y sus ingresos mensuales
+#Muestre en pantalla si el usuario tiene que tributar
+#Declaracion
+edad_usua=0
+ingresos=0.0
+import os
+
+#Input
+edad_usua=int(os.sys.argv[1])
+ingresos=float(os.sys.argv[2])
+
+#Processing
+if (edad_usua>18 and ingresos>=1000):
+    print("Tienes que cotizar")
