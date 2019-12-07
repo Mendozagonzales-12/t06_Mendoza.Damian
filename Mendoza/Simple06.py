006938fb1f57ef992762212f62fbd3106aa70a7e
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-07 16:58:08 -0500

    CONDICIONAL SIMPLE 06

---
+++ Mendoza/Simple06.py
@@ -0,0 +1,12 @@
+#Programa
+#Dado un numero, decir si en menor de 50
+#Declaracion
+nmro=0.0
+import os
+
+#Input
+nmro=int(os.sys.argv[1])
+
+#Processing
+if(nmro<50):
+    print("Numero es menor a 50")
