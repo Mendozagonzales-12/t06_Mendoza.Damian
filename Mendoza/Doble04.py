9f55637c35039e08b4bd34a9676ae80f8ccff558
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-07 20:15:40 -0500

    CONDICIONAL DOBLE 4

---
+++ Mendoza/Doble04.py
@@ -0,0 +1,14 @@
+#Programa que pida al usuario un numero entero
+#Muestre por pantalla si es par
+#Declaracion
+numer=0
+import os
+
+#Input
+numer=int(os.sys.argv[1])
+
+#Processing
+if (numer%2==0):
+    print("El número " + str(numer) + " es par")
+else:
+    print("El numero es impar")
