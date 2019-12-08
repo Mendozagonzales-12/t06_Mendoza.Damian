b040f946d4984ed42f5c9a89fd31d36ce7ce71db
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-07 21:08:23 -0500

    CONDICIONAL MULTIPLE 02

---
+++ Mendoza/Multiple08.py
@@ -0,0 +1,18 @@
+#Programa que pida dos numeros
+#Muestre en pantalla si el primero es mayor
+#Declaracion
+num1,num2=0,0
+import os
+
+#Input
+num1=int(os.sys.argv[1])
+num2=int(os.sys.argv[2])
+
+#Processing
+#si el numero 2>numero 1, indicarlo en pantalla
+if(num1>num2):
+    print("El primer numero",num1," es mayor que ",num2)
+elif(num2>num1):
+    print("Numero ",num2," Es mayor que ",num1)
+elif(num1==num2):
+    print("Numero ",num1," Es igual que ",num2)
