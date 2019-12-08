cd5c38bbdb61af29949806f3b7e81f54df32b05f
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-07 21:01:25 -0500

    CONDICIONAL DOBLE 9

---
+++ Mendoza/Doble09.py
@@ -0,0 +1,28 @@
+#Programa para que calcule el total a pagar por la compra de camisas.
+#Declaracion
+nc=0
+pc=0.0
+import os
+
+#Input
+nc=int(os.sys.argv[1])
+pc=float(os.sys.argv[2])
+
+#Processing
+#Si se compran tres camisas o más se aplica un descuento del 20%
+if (nc >= 3):
+    costo = nc*pc
+    print("El costo de las camisas es ",costo)
+    des = costo*0.20
+    print("El descuento es ",des)
+    pt = costo-des
+    print("El costo total a pagar es ",pt)
+#si son menos de tres camisas un descuento del 10%
+else:
+    costo = nc*pc
+    print("El costo de las camisas es ",costo)
+    des = costo*0.10
+    print("El descuento es ",des)
+    pt = costo-des
+    print("El costo total a pagar es ",pt)
+
