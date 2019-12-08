5182efa129b1970ebd048b325e296dc318592a51
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-07 20:17:10 -0500

    CONDICIONAL DOBLE 6

---
+++ Mendoza/Doble06.py
@@ -0,0 +1,15 @@
+#Programa
+#Dado un numero, decir si en menor de 50
+#Declaracion
+nmro=0.0
+import os
+
+#Input
+nmro=int(os.sys.argv[1])
+
+#Processing
+#si el numero>50 mostar numero mayor a 50
+if(nmro<50):
+    print("Numero es menor a 50")
+else:
+    print("Numero es mayor a 50")
