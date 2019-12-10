f618a4f9ac2cb8265c5c16b2950160b3079bb273
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-10 16:13:49 -0500

    CONDICIONAL doble 3

---
+++ Damian/doble3.py
@@ -0,0 +1,18 @@
+#Calcular el precio de venta del producto
+#Declaracion
+precio_venta=0
+ganancia=0
+precio_compra=0
+import os
+
+#Input
+precio_compra=int(os.sys.argv[1])
+ganancia=int(os.sys.argv[2])
+
+#Processing
+precio_venta=precio_compra+ganancia
+
+if (precio_venta>0):
+    print("el precio de venta es:",precio_compra+ganancia)
+else:
+    print("incorrecto")
