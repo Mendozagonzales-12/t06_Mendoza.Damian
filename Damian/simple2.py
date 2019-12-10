8ac063914fd7d883b1c6a558e816b3c52b8a5c44
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-10 16:08:30 -0500

    CONDICIONAL simple2

---
+++ Damian/simple2.py
@@ -0,0 +1,16 @@
+#Dado los numeros mostrar el promedio
+#Declaracion
+num1=0
+num2=0
+promedio=0
+import os
+
+#Input
+num1=int(os.sys.argv[1])
+num2=int(os.sys.argv[2])
+
+#Procesing
+promedio=num1+num2/2
+
+if (promedio>0):
+    print("el promedio de los numeros es:",num1+num2/2)
