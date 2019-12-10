a71d6e1a76b1f0eb553b7089c10c942b5f8bc278
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-10 16:13:10 -0500

    CONDICIONAL doble1

---
+++ Damian/doble1.py
@@ -0,0 +1,18 @@
+#calcular el area del triangulo
+#Declaracion
+base=0
+altura=0
+area=0
+import os
+
+#Input
+base=int(os.sys.argv[1])
+altura=int(os.sys.argv[2])
+
+#Processing
+area=base*altura/2
+
+if (area>0):
+    print("el area del triangulo es:",base*altura/2)
+else:
+    print("esta mal efectuada")
