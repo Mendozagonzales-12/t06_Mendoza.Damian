cdb01495f699ece9124d4231a650aa7bde539942
Mendozagonzales-12 <57878122+mendozagonzales-12@users.noreply.github.com>
2019-12-10 16:17:41 -0500

    CONDICIONAL MULTIPLE 6

---
+++ Damian/multiple6.py
@@ -0,0 +1,24 @@
+#calcular el mantenimiento de valor cuenta de ahorro
+#Declaracion
+monto_principal=0.0
+tipo_cambio_dia_anterior=0.0
+cambio_dia_hoy=0.0
+mantenimiento_valor_cuenta_de_ahorro=0.0
+import os
+
+#input
+monto_principal=float(os.sys.argv[1])
+tipo_cambio_dia_anterior=float(os.sys.argv[2])
+cambio_dia_hoy=float(os.sys.argv[3])
+
+#Processing
+mantenimiento_valor_cuenta_de_ahorro=(monto_principal/tipo_cambio_dia_anterior*cambio_dia_hoy-monto_principal)
+
+if (mantenimiento_valor_cuenta_de_ahorro>0.0):
+    print("el valor de la cuenta de ahorro es:",monto_principal/tipo_cambio_dia_anterior*cambio_dia_hoy-monto_principal)
+if(mantenimiento_valor_cuenta_de_ahorro>0.5):
+    print("costo minimo")
+if(mantenimiento_valor_cuenta_de_ahorro>1.5):
+    print("costo directo")
+if(mantenimiento_valor_cuenta_de_ahorro>2.5):
+    print("costo indirecto")
